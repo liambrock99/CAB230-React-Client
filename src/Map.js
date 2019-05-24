fda05a554f54c6f87c707348887b2cbb5e86dab1
Liam <liam.brock99@gmail.com>
2019-05-25 00:09:39 +1000

    react-leaflet-heatmap-layer

--- src/Map.js
+++ src/Map.js
@@ -1,32 +1,50 @@
 import React from "react";
-import { Map, Popup, TileLayer, CircleMarker } from "react-leaflet";
+import HeatmapLayer from "react-leaflet-heatmap-layer";
+import {
+  Map,
+  Popup,
+  TileLayer,
+  Marker,
+  LayersControl,
+  FeatureGroup
+} from "react-leaflet";
 
 export default function MapWrapper(props) {
   return (
-    <Map
-      center={props.center}
-      zoom={props.zoom}
-      style={props.style}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {props.data
-        .filter(e => e.lat !== null && e.lng !== null) // remove any entries with null lat/lngs that would break the app
-        .map(e => (
-          <CircleMarker
-            key={e.LGA}
-            center={[e.lat, e.lng]}
-            radius={e.total * 0.001}
-          >
-            <Popup>
-              {e.LGA}
-              <br />
-              Total: {e.total}
-            </Popup>
-          </CircleMarker>
-        ))}
+    <Map center={props.center} zoom={props.zoom} style={props.style}>
+      <LayersControl>
+        <LayersControl.BaseLayer name="Base" checked>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.Overlay name="Heatmap" checked>
+          <FeatureGroup color="Blue">
+            <HeatmapLayer
+              points={props.data}
+              longitudeExtractor={e => e.lng}
+              latitudeExtractor={e => e.lat}
+              intensityExtractor={e => (e.total )}
+            />
+          </FeatureGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Markers">
+          <FeatureGroup>
+            {props.data
+              .filter(e => e.lat !== null && e.lng !== null) // remove any entries with null lat/lngs that would break the app
+              .map(e => (
+                <Marker key={e.LGA} position={[e.lat, e.lng]}>
+                  <Popup>
+                    {e.LGA}
+                    <br />
+                    Total: {e.total}
+                  </Popup>
+                </Marker>
+              ))}
+          </FeatureGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
     </Map>
   );
 }
